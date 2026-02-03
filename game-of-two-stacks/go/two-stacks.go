06e5213d1fee48130dff6923b216708e6a6bcb7c
max2sax <max2sax_work@outlook.com>
2026-02-02 22:28:44 -0500

    add variation

--- game-of-two-stacks/go/two-stacks.go
+++ game-of-two-stacks/go/two-stacks.go
@@ -2,6 +2,7 @@ package gameoftwostacks
 
 import (
 	"fmt"
+	"log"
 )
 
 func twoStacks(maxSum int32, a []int32, b []int32) int32 {
@@ -42,17 +43,39 @@ func twoStacksNew(maxSum int32, a []int32, b []int32) int32 {
 	var selections int32
 	curSum := int32(0)
 	for {
-		fmt.Printf("[%d,%d]: ", i, j)
-		if i < maxI && (j == maxJ || a[i] <= b[j]) {
-			fmt.Printf("choosing a : %d", a[i])
-			curSum += a[i]
-			i++
-		} else if j < maxJ && (i == maxI || b[j] < a[i]) {
-			fmt.Printf("choosing b: %d", b[j])
-			curSum += b[j]
-			j++
+		log.Printf("[%d,%d]: ", i, j)
+		nextA := maxSum + 1
+		nextB := maxSum + 1
+		if i < maxI {
+			nextA = a[i]
 		}
-		fmt.Printf(", CurSum: %d\n", curSum)
+		if j < maxJ {
+			nextB = b[j]
+		}
+		nextSumA := curSum + nextA
+		nextSumB := curSum + nextB
+		if nextSumA > maxSum && nextSumB > maxSum {
+			break
+		}
+
+		log.Printf("nextA: %d, nextB: %d; nextSumA: %d, nextSumB: %d; ", nextA, nextB, nextSumA, nextSumB)
+		i++
+		curSum = nextSumA
+		if nextSumB < nextSumA {
+			i--
+			j++
+			curSum = nextSumB
+		}
+		// if  {
+		// 	fmt.Printf("choosing a : %d", a[i])
+		// 	curSum += a[i]
+		// 	i++
+		// } else if j < maxJ && (i == maxI || b[j] < a[i]) {
+		// 	fmt.Printf("choosing b: %d", b[j])
+		// 	curSum += b[j]
+		// 	j++
+		// }
+		log.Printf(", CurSum: %d\n", curSum)
 		if curSum > maxSum {
 			break
 		}
--- game-of-two-stacks/go/two-stacks_test.go
+++ game-of-two-stacks/go/two-stacks_test.go
@@ -21,3 +21,19 @@ func TestTwoStacks(t *testing.T) {
 		})
 	}
 }
+func TestNewTwoStacks(t *testing.T) {
+	maxT := len(tests)
+
+	maxT = 5
+	for i := 4; i < maxT; i++ {
+		test := tests[i]
+		tName := fmt.Sprintf("%d:  ", i)
+		t.Run(tName, func(t *testing.T) {
+			expected := outputs01[i]
+			result := twoStacksNew(test.maxSum, test.a, test.b)
+			if result != expected {
+				t.Errorf("twoStacksNew(%d, \n%v, \n%v) \n= %d; want %d", test.maxSum, test.a, test.b, result, expected)
+			}
+		})
+	}
+}
