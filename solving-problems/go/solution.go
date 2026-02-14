d2f11f37754916faa8b7e053e916bfc9f3aca0a1
max2sax <max2sax_work@outlook.com>
2026-02-13 20:31:23 -0500

    add subarray and naive graph approach. neither work.

--- solving-problems/go/solution.go
+++ solving-problems/go/solution.go
@@ -1,6 +1,7 @@
 package solvingproblems
 
 import (
+	"fmt"
 	"log"
 )
 
@@ -12,10 +13,10 @@ func problemSolving(k int32, v []int32) int32 {
 	// log.Println("starting v on day 1  :", v)
 	for {
 		unsolved := v[:0]
-		prevRating := v[0]
+		lastSolvedRating := v[0]
 		for _, rating := range v[1:] {
-			diff := rating - int32(prevRating)
-			negDiff := int32(prevRating) - rating
+			diff := rating - int32(lastSolvedRating)
+			negDiff := int32(lastSolvedRating) - rating
 			if diff < 0 {
 				diff = negDiff
 			}
@@ -23,7 +24,7 @@ func problemSolving(k int32, v []int32) int32 {
 				unsolved = append(unsolved, rating)
 				continue
 			}
-			prevRating = rating
+			lastSolvedRating = rating
 		}
 		if len(unsolved) == 0 {
 			break
@@ -34,4 +35,95 @@ func problemSolving(k int32, v []int32) int32 {
 		days++
 	}
 	return days
+}
+
+type t struct {
+	index int
+	val   int32
+}
+type pair struct {
+	start t
+	end   t
+}
+
+func problemSolvingRecursiveGraph(k int32, v []int32) int32 {
+	// any consecutive problem less than k apart cannot be solved on the same day
+	// solving something on one day, could create unsolvable adjacencies the next day
+	// options, pick subarrays to solve, pick individual elements to solve, skipping around
+	// always have to pick from left to right(lowest index to highest index)
+	// let's try creating subarays to solve and then grouping the subarrays until it's not possible
+	if len(v) < 2 {
+		return 1
+	}
+	minusK := k * -1
+	days := make([][]int32, 0, len(v)) // treating days like a stack?
+	days = append(days, []int32{v[0]})
+	fmt.Println(v)
+	fmt.Println(" ________")
+	for _, rating := range v[1:] {
+		foundDay := false
+		// check if the last solved problem in each day is < k
+		for i, d := range days {
+			lastDayRating := d[len(d)-1]
+			diff := lastDayRating - rating
+			if diff < k && diff > minusK {
+				// too close, check next day
+				continue
+			}
+			// found a day, so add it here
+			days[i] = append(d, rating)
+			foundDay = true
+			break
+		}
+		if foundDay {
+			continue
+		}
+		days = append(days, []int32{rating})
+	}
+	for _, d := range days {
+		fmt.Println(d)
+	}
+	return int32(len(days))
+}
+
+func problemSolvingSubArrays(k int32, v []int32) int32 {
+	// any consecutive problem less than k apart cannot be solved on the same day
+	// solving something on one day, could create unsolvable adjacencies the next day
+	// options, pick subarrays to solve, pick individual elements to solve, skipping around
+	// always have to pick from left to right(lowest index to highest index)
+	// let's try creating subarays to solve and then grouping the subarrays until it's not possible
+	if len(v) < 2 {
+		return 1
+	}
+	minusK := k * -1
+	days := make([][]int32, 0, len(v)) // treating days like a stack?
+	days = append(days, []int32{v[0]})
+	fmt.Println(v)
+	fmt.Println(" ________")
+	problemGroups := make([]pair, 0)
+
+	// build the groups
+
+	firstPair := pair{start: t{0, v[0]}}
+	for i := range v[:len(v)-1] {
+		curRating := v[i]
+		nextRating := v[i+1]
+		diff := curRating - nextRating
+		if diff < k && diff > minusK {
+			// too close, check next day
+			firstPair.end = t{i, v[i]}
+			problemGroups = append(problemGroups, firstPair)
+
+			firstPair.start = t{i + 1, v[i+1]}
+		}
+	}
+	// can't join 2 groups next to each other,
+	// but it might be possible to join groups not next to each other
+	for _, p := range problemGroups {
+		fmt.Println("pairx : ", p)
+	}
+	for _, d := range days {
+		fmt.Println(d)
+	}
+	return int32(len(problemGroups))
 }
